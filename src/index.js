8b4850575bad749200bb9396e517b3c6ab5ce856
ak <andrey.kalinichenko@gmail.com>
2020-01-15 21:35:47 +0100

    t

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
-const Telegraf = require("telegraf");
+require("dotenv").config();
 const express = require("express");
+import { scanFlights } from "./flights";
+// import { bot } from "./bot";
+
+const url = process.env.APP_URL;
+
+export const bot = new Telegraf(TOKEN);
+bot.telegram.setWebhook(`${url}/bot${TOKEN}`);
 
 const port = process.env.PORT;
 const TOKEN = process.env.TELEGRAM_TOKEN;
-const url = process.env.APP_URL;
-
-const bot = new Telegraf(TOKEN);
-// Set the bot response
-bot.on("text", ({ replyWithHTML }) => replyWithHTML("<b>Hello</b>"));
-
-// Set telegram webhook
-// npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook(`${url}/secret-path`);
-
 const app = express();
-app.get("/", (req, res) => res.send("Hello World!"));
-// Set the bot API endpoint
-app.use(bot.webhookCallback("/secret-path"));
-app.listen(port, () => {
-  console.log("Example app listening on port 3000!");
-});
 
-// No need to call bot.launch()
+app.use(bot.webhookCallback(`/bot${TOKEN}`));
+app.get(`/flights`, async (_, res) => {
+  try {
+    await scanFlights();
+    res.sendStatus(200);
+  } catch (e) {
+    res.sendStatus(500, "Refresh flights failed");
+  }
+});
+app.get(`/`, (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+// getRyanairFlight({
+//   origin: "SXF",
+//   destination: "KBP",
+//   departureDateMin: "2020-02-01",
+//   departureDateMax: "2020-02-10",
+//   arrivalDateMin: "2020-02-20",
+//   arrivalDateMax: "2020-02-27"
+// });
